7b6055b1863d0bbfa8deb5903f8e62f35351cfd7
Matt Hand <mmhand123@gmail.com>
2025-10-05 19:16:02 -0400

    got build on save working

---
+++ packages/native/src/root.zig
@@ -0,0 +1,34 @@
+//! By convention, root.zig is the root source file when making a library.
+const std = @import("std");
+
+pub fn main() !void {
+    try std.fs.File.stdout().writeAll("Hello, Native!\n");
+
+    const dirname = std.fs.path.dirname(@src().file) orelse ".";
+
+    const abs = try std.fs.path.resolve(std.heap.page_allocator, dirname);
+    defer std.heap.page_allocator.free(abs);
+
+    std.debug.print("dirname: {s}\n", .{abs});
+}
+
+pub fn bufferedPrint() !void {
+    // Stdout is for the actual output of your application, for example if you
+    // are implementing gzip, then only the compressed bytes should be sent to
+    // stdout, not any debugging messages.
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    const stdout = &stdout_writer.interface;
+
+    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+
+    try stdout.flush(); // Don't forget to flush!
+}
+
+pub fn add(a: i32, b: i32) i32 {
+    return a + b;
+}
+
+test "basic add functionality" {
+    try std.testing.expect(add(3, 7) == 10);
+}
