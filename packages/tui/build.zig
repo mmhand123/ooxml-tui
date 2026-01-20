86baa84059b2b847f156d5ed6b848f0ee1cc914f
Matt Hand <mmhand123@gmail.com>
2026-01-20 14:56:15 -0500

    better setup for build zig

--- packages/tui/build.zig
+++ packages/tui/build.zig
@@ -1,46 +1,37 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const mod = b.addModule("native", .{
-        .root_source_file = b.path("src/root.zig"),
+        .root_source_file = b.path("src/native/root.zig"),
         .target = target,
+        .optimize = optimize,
     });
 
-    const exe = b.addExecutable(.{
-        .name = "native",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-            .imports = &.{
-                .{ .name = "native", .module = mod },
-            },
-        }),
+    const lib = b.addLibrary(.{
+        .name = "ooxml-tui",
+        .root_module = mod,
+        .linkage = .dynamic,
     });
 
-    b.installArtifact(exe);
+    const install_dir = b.addInstallArtifact(lib, .{ .dest_dir = .{ .override = .{
+        .custom = try std.fmt.allocPrint(b.allocator, "./dist/ooxml-tui", .{}),
+    } } });
 
-    const exe_check = b.addExecutable(.{
+    const lib_check = b.addExecutable(.{
         .name = "check",
         .root_module = mod,
     });
 
     const check = b.step("check", "Check if the module compiles");
 
-    check.dependOn(&exe_check.step);
+    check.dependOn(&lib_check.step);
 
-    const run_step = b.step("run", "Run the app");
+    const build_step = b.step("build", "Build the module");
+    build_step.dependOn(&install_dir.step);
 
-    const run_cmd = b.addRunArtifact(exe);
-    run_step.dependOn(&run_cmd.step);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+    b.getInstallStep().dependOn(&install_dir.step);
 
     const mod_tests = b.addTest(.{
         .root_module = mod,
@@ -48,13 +39,6 @@ pub fn build(b: *std.Build) void {
 
     const run_mod_tests = b.addRunArtifact(mod_tests);
 
-    const exe_tests = b.addTest(.{
-        .root_module = exe.root_module,
-    });
-
-    const run_exe_tests = b.addRunArtifact(exe_tests);
-
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_mod_tests.step);
-    test_step.dependOn(&run_exe_tests.step);
 }
