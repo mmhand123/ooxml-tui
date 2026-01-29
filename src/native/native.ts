fedc57cdadc8c31da8c7d10e0bc1a0bf04c84386
Matt Hand <mmhand123@gmail.com>
2026-01-29 13:20:24 -0500

    docs

--- src/native/native.ts
+++ src/native/native.ts
@@ -1,3 +1,10 @@
+/**
+ * This file exposes the native API to the Typescript side.
+ * Anything we `export` in `./root.zig` needs to have a corresponding
+ * definition here, and then needs to be wrapped properly (with validation)
+ * to be exposed to the Typescript side
+ *
+ */
 import { dlopen, CString } from "bun:ffi";
 
 const native = dlopen("./zig-out/dist/ooxml-tui/libooxml-tui.so", {
